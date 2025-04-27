0d9af0505ac1cc0a46cfa96ad549ab162ab0ca07
xiuxiu97 <862812375@qq.com>
2025-04-27 22:21:39 +0800

    修改中继服务器域名和key和自定义密码

--- src/config.rs
+++ src/config.rs
@@ -70,7 +70,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
         pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), ".".to_string());
+        map.insert("password".to_string(), "Qgwdnbhl6.".to_string());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["re..asia"];
-pub const RS_PUB_KEY: &str = "=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rockcken.top"];
+pub const RS_PUB_KEY: &str = "60K5W7y7uFnmUhDZx4+4t1kUaHbYVdAV5DNaO4X0gbA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
